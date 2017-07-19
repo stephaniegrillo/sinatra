097aff1a2e94efc98bceae3789839d46907398b5
Stephanie <stephaniegrillo329@gmail.com>
2017-07-19 11:03:29 -0400

    first Heroku commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+ruby "2.4.1"
+gem "sinatra"
+
+
+web ruby server -p $PORT
---
+++ server.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+	"Hello world"
+end
+
+get "/sinatra" do
+	"Hello Sinatra"
+end
+
+get "/is" do
+	"Hola Sinatra"
+end
